15d7c73b9f010f1078e3b820b3d09622f9b622f5
Lynch the Lionheart <elatedpixel@gmail.com>
2023-11-30 20:59:13 -0500

    updates after chatgpt code review

--- 2018/day01/src/main.rs
+++ 2018/day01/src/main.rs
@@ -3,33 +3,34 @@ use std::{collections::HashSet, fs};
 // beginning with frequency `0`, return the result of applying the frequency
 // changes in the input string, which contain both positive and negative numbers
 // separated by newlines (and a blank line at the end).
-fn part_one(input: &String) -> i32 {
-    input
-        .trim()
-        .lines()
-        .map(|s| i32::from_str_radix(s, 10).unwrap_or(0))
-        .sum()
+fn part_one(input: &str) -> i32 {
+    input.trim().lines().map(parse_frequency_change).sum()
 }
 
 // beginning with frequency `0`, find the first frequency that occurs twice
-fn part_two(input: &String) -> i32 {
-    let mut value = 0;
-    let mut seen: HashSet<i32> = HashSet::from([value]);
-    for line in input.trim().lines().cycle() {
-        let x = i32::from_str_radix(line, 10).unwrap_or(0);
-        value += x;
-        if seen.contains(&value) {
-            return value;
+fn part_two(input: &str) -> i32 {
+    let mut frequency = 0;
+    let mut seen: HashSet<i32> = HashSet::from([frequency]);
+    for change in input.trim().lines().map(parse_frequency_change).cycle() {
+        frequency += change;
+        if seen.contains(&frequency) {
+            return frequency;
         } else {
-            seen.insert(value);
+            seen.insert(frequency);
         }
     }
     panic!("part_two() didn't find any frequency twice!");
 }
 
+fn parse_frequency_change(change_string: &str) -> i32 {
+    change_string
+        .parse::<i32>()
+        .expect("All change strings should contain integers!")
+}
+
 fn main() {
     let input =
         fs::read_to_string("src/input").expect("Input file should be in the src directory!");
-    println!("2018 Day01 Part01: Frequency: {:?}", part_one(&input));
-    println!("2018 Day01 Part02: Frequency: {:?}", part_two(&input));
+    println!("2018 Day01 Part01: Frequency: {}", part_one(&input));
+    println!("2018 Day01 Part02: Frequency: {}", part_two(&input));
 }
